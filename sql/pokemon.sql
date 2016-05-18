d6219815b33200b535d5d41b8caa8a94534960e2
censhin <censhin@gmail.com>
2016-05-17 23:13:01 -0500

    feat(sql): add queries for abilities and effort_values

--- sql/pokemon.sql
+++ sql/pokemon.sql
@@ -1,11 +1,14 @@
-SELECT poke.identifier  AS name
-     , poke.species_id  AS number
-     , gen_names.name   AS generation
-     , typ.types        AS types
-     , spec_names.genus AS genus
-     , poke.height
-     , poke.weight
-     , poke.base_experience
+SELECT poke.identifier           AS name
+     , poke.species_id           AS number
+     , gen_names.name            AS generation
+     , typ.types                 AS types
+     , spec_names.genus          AS genus
+     , poke.height               AS height
+     , poke.weight               AS weight
+     , species.capture_rate      AS capture_rate
+     , poke.base_experience      AS base_experience
+     , poke_abilities.identifier AS abilities
+     , ev.ev_data                AS effort_values
   FROM pokemon AS poke
   JOIN pokemon_species AS species
     ON poke.identifier = species.identifier
@@ -30,15 +33,24 @@ SELECT poke.identifier  AS name
          WHERE local_language_id = 9
        ) AS spec_names
     ON spec_names.pokemon_species_id = poke.species_id
+  JOIN (
+        SELECT string_agg(abil.identifier, ', ') AS identifier
+             , poke_abil.pokemon_id
+          FROM pokemon_abilities as poke_abil
+          JOIN abilities as abil
+            ON poke_abil.ability_id = abil.id
+         GROUP BY poke_abil.pokemon_id
+       ) AS poke_abilities
+    ON poke_abilities.pokemon_id = poke.species_id
+  JOIN (
+        SELECT poke_stats.pokemon_id
+             , concat(stats.identifier, ', ', poke_stats.effort) AS ev_data
+          FROM pokemon_stats AS poke_stats
+          JOIN stats
+            ON stats.id = poke_stats.stat_id
+         WHERE poke_stats.effort > 0
+       ) AS ev
+    ON ev.pokemon_id = poke.species_id
  WHERE poke.is_default = 't'
    AND gen_names.local_language_id = 9
  ORDER BY poke.species_id;
-
-
-SELECT poke.identifier AS name
-     , typ.identifier  AS types
-  FROM pokemon_types   AS poke_types
-  JOIN pokemon AS poke
-    ON poke_types.pokemon_id = poke.id
-  JOIN types AS typ
-    ON poke_types.type_id = typ.id;
